dad88dd3a8174a325b757854264dd2355fbe6a64
pathllk1 <pathllk@hotmail.com>
2021-03-25 01:26:50 +0530

    24-03-2021 1

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 
-import  { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { Grid, Paper, TextField, Typography, makeStyles, Button, Avatar } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
@@ -33,72 +33,83 @@ const Login = () => {
     const [username, setusername] = useState('');
     const [password, setpassword] = useState('');
     const [message, setmessage] = useState('');
+    const [currentUser, setcurrentUser] = useState(undefined);
 
-    function onSubmit(e){
+    useEffect(() => {
+        const user = AuthService.getCurrentUser();
+        if (user) {
+            setcurrentUser(user);
+        }
+    });
+
+    function onSubmit(e) {
         e.preventDefault()//blocks the postback event of the page
         AuthService.login(username, password).then(
             () => {
-              window.location.reload();
+                window.location.reload();
             },
             error => {
-              const resMessage =
-                (error.response &&
-                  error.response.data &&
-                  error.response.data.message) ||
-                error.message ||
-                error.toString();
-    
-              setmessage(resMessage);
+                const resMessage =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+
+                setmessage(resMessage);
             }
-          );
+        );
     }
     return (
         <Grid>
             <Paper elevation={10} className={classes.paperStyle}>
-                <Grid align='center'>
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">Sign in</Typography>
-                    <form className={classes.form} noValidate onSubmit={onSubmit}>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="username"
-                            label="User Name"
-                            name="username"
-                            autoComplete="username"
-                            autoFocus
-                            value={username}
-                            onChange={e => setusername(e.target.value)}
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            value={password}
-                            onChange={e => setpassword(e.target.value)}
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                        >
-                            Sign In
+                {currentUser ? (
+                    <h4>Logged in as {currentUser.username}</h4>
+                ) : (
+                    <Grid align='center'>
+                        <Avatar className={classes.avatar}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">Sign in</Typography>
+                        <form className={classes.form} noValidate onSubmit={onSubmit}>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="username"
+                                label="User Name"
+                                name="username"
+                                autoComplete="username"
+                                autoFocus
+                                value={username}
+                                onChange={e => setusername(e.target.value)}
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                value={password}
+                                onChange={e => setpassword(e.target.value)}
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Sign In
           </Button>
-                    </form>
-                    <input type='text' value={message}/>
-                </Grid>
+                        </form>
+                    </Grid>
+                )}
             </Paper>
         </Grid>
 
